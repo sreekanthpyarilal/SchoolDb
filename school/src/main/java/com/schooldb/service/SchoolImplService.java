e3ca6a5bde86b4bcdaa8d7fb93149381ac514f1f
sreekanthpyarilal <sreekanthpyarilal@gmail.com>
2023-05-19 14:35:58 +0530

    user exist check

--- school/src/main/java/com/schooldb/service/SchoolImplService.java
+++ school/src/main/java/com/schooldb/service/SchoolImplService.java
@@ -1,6 +1,8 @@
 package com.schooldb.service;
 
 import com.schooldb.exception.DataAlreadyExistException;
+import com.schooldb.model.Employee;
+import com.schooldb.repository.EmpRepo;
 import com.schooldb.repository.SchoolRepo;
 import com.schooldb.model.School;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,26 +10,45 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
-public class SchoolImplService implements SchoolService{
+public class SchoolImplService implements SchoolService {
 
     @Autowired
     SchoolRepo schoolRepo;
 
+    @Autowired
+    EmpRepo empRepo;
+
     @Override
     public School saveSchoolData(School school) throws DataAlreadyExistException {
-
-        Optional<School> optional= schoolRepo.findById((school.getSchoolId()));
-        if(optional.isPresent()){
-            throw new DataAlreadyExistException("user already exist");
+        boolean emp_exist=false;
+        Optional<School> optional = schoolRepo.findById((school.getSchoolId()));
+        if (optional.isPresent()) {
+            ArrayList<Integer> emp_ids = new ArrayList<>();
+            List<Employee> emp = school.getEmployees();
+            for (Employee id : emp) {
+                Optional<Employee> ids = empRepo.findById(id.getEmpId());
+                if (ids.isPresent()) {
+                    System.out.println("emp already exist" + id.getEmpId());
+                    emp_exist=true;
+                } else {
+                    return schoolRepo.save(school);
+                }
+            }
+        }
+        if(emp_exist){
+            throw new DataAlreadyExistException("User already exist");
         }
         return schoolRepo.save(school);
+
     }
+
 
     @Override
     public ArrayList<School> getAllSchooldata() {
         return (ArrayList<School>) schoolRepo.findAll();
     }
-
-    }
+}
